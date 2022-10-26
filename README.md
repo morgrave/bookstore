7f7d1d6158cbee1d954aaf9ee0eb14e90e731fe1
dwaldo <dwaldo@naver.com>
2022-10-26 17:28:58 +0900

    안쓰는 로그 날림

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Bookshelf
+# Bookstore
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.1.1.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('bookshelf app is running!');
+    expect(await page.getTitleText()).toEqual('bookstore app is running!');
   });
 
   afterEach(async () => {
