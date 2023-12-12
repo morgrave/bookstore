ea9790613e5f5168c6c73b0884786d7fdb40cccb
dwaldo <dwaldo@naver.com>
2023-12-12 16:43:23 +0900

    giscus

--- src/app/viewer/components/viewer-text.component.ts
+++ src/app/viewer/components/viewer-text.component.ts
@@ -15,6 +15,22 @@ export class ViewerTextComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    let node = document.createElement('script');
+    node.dataset["repo"]="morgrave/xperion"
+    node.dataset["repoId"]="R_kgDOIl1gMg"
+    node.dataset["category"]="General"
+    node.dataset["categoryId"]="DIC_kwDOIl1gMs4CbrQG"
+    node.dataset["mapping"]="pathname"
+    node.dataset["strict"]="0"
+    node.dataset["reactionsEnabled"]="0"
+    node.dataset["emitMetadata"]="0"
+    node.dataset["inputPosition"]="bottom"
+    node.dataset["theme"]="preferred_color_scheme"
+    node.dataset["lang"]="ko"
+    node.src = "https://giscus.app/client.js";
+    node.type = 'text/javascript';
+    node.async = true;
+    document.getElementsByClassName('comment')[0].appendChild(node);
   }
 
 }
